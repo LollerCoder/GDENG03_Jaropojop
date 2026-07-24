979ff3ae84711b49e888400e12b40d759424a56e
LollerCoder <jairiojaps@gmail.com>
2026-07-24 16:45:38 +0800

    SomeCleaning

--- AppWindow.cpp
+++ AppWindow.cpp
@@ -77,12 +77,7 @@ void AppWindow::update()
 	float aspect = (float)(this->getClientWindowRect().right - this->getClientWindowRect().left) /
 		(float)(this->getClientWindowRect().bottom - this->getClientWindowRect().top);
 
-	/*cc.m_proj.setOrthoLH(
-		2.0f * aspect,
-		2.0f,
-		-4.0f,
-		4.0f
-	);*/
+
 
 	cc.m_proj.setPerspectiveFovLH(1.57f,aspect,0.1f,100.0f);
 	m_cb->update(GraphicsEngine::get()->getImmediateDeviceContext(), &cc);
@@ -124,35 +119,22 @@ void AppWindow::onUpdate()
 	GraphicsEngine::get()->getImmediateDeviceContext()->setViewportSize(rc.right - rc.left, rc.bottom - rc.top);
 
 	
-	/*ImGui_ImplDX11_NewFrame();
-	ImGui_ImplWin32_NewFrame();
-	ImGui::NewFrame();
-	ImGui::ShowDemoWindow();*/
+	
 	
 
 	update();
 	
 	
-	//relevant to draws
 	
-	/*for (Quads* q : quadList) {
-		q->setConstantBuffer(m_cb);
-		q->draw();
-	}*/
 	plane->setConstantBuffer(m_cb);
 	plane->draw();
 	cubeWork->setConstantBuffer(m_cb);
 	cubeWork->draw();
 
-	/*for (Cube* c : cubeList) {
-		c->setConstantBuffer(m_cb);
-		c->draw();
-	}*/
-
+	
 	UIManager::getInstance()->drawAllUI();
 
-	/*ImGui::Render();
-	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());*/
+	
 	m_swap_chain->present(false);
 	
 }
@@ -167,11 +149,7 @@ void AppWindow::onDestroy()
 	}
 
 	m_cb->Release();
-	////IMGUI
-	//ImGui_ImplDX11_Shutdown();
-	//ImGui_ImplWin32_Shutdown();
-	//ImGui::DestroyContext();
-
+	
 	Window::onDestroy();
 	m_swap_chain->release();
 	GraphicsEngine::get()->destroy();
@@ -192,7 +170,7 @@ void AppWindow::onKillFocus()
 
 void AppWindow::CreateGraphicsWindow()
 {
-	srand((unsigned int)time(nullptr));
+	
 
 
 	
@@ -203,7 +181,6 @@ void AppWindow::CreateGraphicsWindow()
 
 
 
-	//GraphicsEngine::get()->init();
 	GraphicsEngine::get()->initialize();
 
 	m_swap_chain = GraphicsEngine::get()->createSwapChain();
@@ -239,17 +216,7 @@ void AppWindow::CreateGraphicsWindow()
 
 	cubeWork = new Cube();
 	cubeList.push_back(cubeWork);
-	//Answer for #4 render 50 random cubes
-	/*for (int i = 0; i < 50; i++)
-	{
-		float x = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
-		float y = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
-		float z = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
-
-		Cube* cube = new Cube(x, y, z);
-		cubeList.push_back(cube);
-	}*/
-
+	
 	constant cc;
 	cc.m_angle = 0;
 
